7488499e9b3466821226f5cac59b39f2339c9d99
Larissa Ribeiro de Souza <souzalarissa383@gmail.com>
2019-05-01 14:14:48 -0300

    Update JogadorGoleiro.java

--- src/JogadorGoleiro.java
+++ src/JogadorGoleiro.java
@@ -2,9 +2,11 @@
 public class JogadorGoleiro extends Jogador{
     private int reflexos;
     private float altura;
+    private int habilidade;
     
-    public int gethabilidades() {
-       return this.habilidades;
+   
+    public int gethabilidades(int habilidade) {
+       return ((habilidade*5) + (((int)(altura*100))*2) + (reflexos*3))/10;
     }
     public void JogadorGoleiro(String nome, int idade, int habilidades, int reflexos,  float altura){
         
